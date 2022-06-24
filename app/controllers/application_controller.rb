154d9b55c5c74b936fbec47dff43b39f022bd9d1
Robinson Idagiza <robinadoro@gmail.com>
2022-06-24 13:30:59 +0300

    ft:completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < Sinatra::Base
-  set :default_content_type, 'application/json'
+  set :default_content_type, "application/json"
 
   get '/games' do
-    games = Game.all.order(:title).limit(10)
-    games.to_json
+    games = Game.all.order(:title).limit(10).to_json
   end
 
   get '/games/:id' do
@@ -14,6 +13,31 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
